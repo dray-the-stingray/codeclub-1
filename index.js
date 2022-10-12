5e3c5e8cbb1a421a0f4cc6154ebf4ae849a7d451
Daniel Ray <dray@cloudflare.com>
2022-10-12 20:40:49 +1100

    completed code

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
+	fetch(request) {
+		console.log("codeclub-1 worker")
+		console.log(request.url);
+		if(request.url == "https://www.cloudflareworkers.com/yes") {
+			if (request.method == 'GET') {
+				return new Response('Yes! GET', {
+					headers: {
+						'content-type': 'text/plain',
+					},
+				});
+			}
+			else if (request.method == 'POST') {
+				return new Response('Yes! POST', {
+					headers: {
+						'content-type': 'application/json',
+					},
+				});
+			}
+			else {
+				return new Response('Yes! Something else..', {
+					headers: {
+						'content-type': 'text/plain',
+					},
+				});
+			}
+		}
+		else {
+			return new Response('Nope!', {
+				headers: {
+					'content-type': 'text/plain'
+				},
+			});
+		}
 	},
 };
